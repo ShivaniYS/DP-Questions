d4c74171cfc2212c51436f0c2e8beb0844c30e70
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2022-09-30 22:53:31 +0530

    Create  Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ Longest common subsequence/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        string B = s;
+        reverse(s.begin(),s.end());
+        int dp[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0) dp[i][j] = 0;
+                
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == B[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+       return n - dp[n][n];  
+    }
+};
