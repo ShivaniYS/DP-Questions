be2427fa8b539addfed6fe9ca6530adc1c88dea3
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2022-10-03 15:31:24 +0530

    Create Diameter of binary Tree.cpp

---
+++ DP ON TREE/Diameter of binary Tree.cpp
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/diameter-of-binary-tree/
+class Solution {
+public:
+    int solve(TreeNode * root,int &res){
+        if(root==NULL)
+        return 0;
+        int l = solve(root->left,res);
+        int r = solve(root->right,res);
+        int temp = max(l,r) + 1;
+        int ans = max(temp,1+l+r);
+        
+         res = max(res,ans);
+       return temp;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = INT_MIN;
+        solve(root,res);
+        return res-1;
+    }
+};
